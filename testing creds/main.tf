a9508b035c6855f038773981cac34d6913d66049
Sheharyar100 <149365770+Sheharyar100@users.noreply.github.com>
2025-01-16 14:34:21 +0500

    Delete testing creds directory

--- testing creds/main.tf
+++
@@ -1,7 +0,0 @@
-module "engine" {
-    source = "./modules/engine"
-    zone = var.zone
-    machineType = var.machineTypeForComputeInstance
-    instanceName = var.instanceNameForComputeInstance
-    imageName = var.imageNameForComputeInstance
-}
--- testing creds/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-    credentials = file("./anifest-ocean-440312-h9-6cee7d6e34b0.json")
-    project = var.projectId
-    region = var.region
-}
